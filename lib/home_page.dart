5fb7e2a20271f66bc01b006bc69619917f4c9027
homerjay <homerjay@naver.com>
2021-01-22 16:20:59 +0900

    TextController,onSubmitted,controller.claer

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatefulWidget {                  //항상 변화하는 채팅앱이기때문에
+class HomePage extends StatefulWidget {
+  //항상 변화하는 채팅앱이기때문에
   @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
+  TextEditingController _textEditingController = TextEditingController();
+  List<String> _mytext;
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -18,29 +21,45 @@ class _HomePageState extends State<HomePage> {
             title: Text('Chat App'),
           ),
           body: Container(
-            padding: EdgeInsets.symmetric(horizontal: 8.0),               //호리존탈 가로길이
+            padding: EdgeInsets.symmetric(horizontal: 8.0), //호리존탈 가로길이
             child: Row(
               children: [
                 Expanded(
-                  child: TextField(                                     //텍스트필드 쓰면 텍스트랑 키보드나옴
+                  child: TextField(
+                    //텍스트필드 쓰면 텍스트랑 키보드나옴
+                    controller: _textEditingController,
+                    //텍스트필드에 들어가있는 텍스트를 플랫버튼을 누르면 print하게
+                    onSubmitted: _handleSubmitted, //Keyboard 판에서 ok를 눌렀을때도 데이터를 보낸다
                     decoration: InputDecoration(
-                    hintText: '메세지를 입력해주세요',
-                  ),
+                      hintText: '메세지를 입력해주세요',
+                    ),
                     keyboardType: TextInputType.text,
                     textAlign: TextAlign.center,
                   ),
                 ),
-                SizedBox(width: 8.0,),
-                FlatButton(onPressed: () {
-
-                }, child: Text('Send'),
-                color: Colors.amberAccent,)
+                SizedBox(
+                  width: 8.0,
+                ),
+                FlatButton(
+                  onPressed: () {
+                    // print(_textEditingController.text);
+                    _handleSubmitted(_textEditingController.text);
+                  },
+                  child: Text('Send'),
+                  color: Colors.amberAccent,
+                )
               ],
             ),
           ),
-
         ),
       ),
     );
+  }
+
+
+
+  void _handleSubmitted(String text) {
+    print(text);
+    _textEditingController.clear();               //텍스트에디터를 클리어해주어 없애준다.
   }
 }
