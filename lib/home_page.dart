962a617d7e36bad17b7c4eebe58321b267428c3b
homerjay <homerjay@naver.com>
2021-01-27 16:39:25 +0900

    채팅내용이 없으면 안보냄,ios이면 cupertino디자인으로

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_chat/chat_message.dart';
 import 'package:logger/logger.dart';
@@ -10,8 +13,9 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   TextEditingController _textEditingController = TextEditingController();
-  List<String> _chats =[];                       // 메세지를 담을 수 있는 전용 클래스
-  GlobalKey<AnimatedListState> _animListKey = GlobalKey<AnimatedListState>(); //애니메이트 상태를 가지고있다
+  List<String> _chats = []; // 메세지를 담을 수 있는 전용 클래스
+  GlobalKey<AnimatedListState> _animListKey =
+      GlobalKey<AnimatedListState>(); //애니메이트 상태를 가지고있다
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -20,18 +24,23 @@ class _HomePageState extends State<HomePage> {
       },
       child: SafeArea(
         child: Scaffold(
-          appBar: AppBar(
-            title: Text('Chat App'),
-          ),
+          appBar: Platform.isIOS
+              ? //현재 플랫폼이 IOS이면 쿠퍼티노 네비게이션바로 표현
+              CupertinoNavigationBar(
+                  middle: Text('Chat App IOS'),
+                )
+              : AppBar(
+                  title: Text('Chat App'),
+                ),
           body: Column(
             children: [
               Expanded(
-              child: AnimatedList(
-                key: _animListKey,
-                itemBuilder: _buildItem,
-                // initialItemCount: _chats.length,
-                // child: ListView.builder(
-                  reverse: true,                     //채팅이 아래에서부터 위로 올라온다.
+                child: AnimatedList(
+                  key: _animListKey,
+                  itemBuilder: _buildItem,
+                  // initialItemCount: _chats.length,
+                  // child: ListView.builder(
+                  reverse: true, //채팅이 아래에서부터 위로 올라온다.
                   // itemBuilder: (context, index) {
                   //   return _chats[index];             //입력한 textController를 리스트에 담아서 출력한다.
                   // },
@@ -48,29 +57,53 @@ class _HomePageState extends State<HomePage> {
                 child: Row(
                   children: [
                     Expanded(
-                      child: TextField(
-                        //텍스트필드 쓰면 텍스트랑 키보드나옴
-                        controller: _textEditingController,
-                        //텍스트필드에 들어가있는 텍스트를 플랫버튼을 누르면 print하게
-                        onSubmitted: _handleSubmitted, //Keyboard 판에서 ok를 눌렀을때도 데이터를 보낸다
-                        decoration: InputDecoration(
-                          hintText: '메세지를 입력해주세요',
-                        ),
-                        textAlign: TextAlign.center,
-                      ),
+                      child: Platform.isIOS
+                          ? CupertinoTextField(
+                              //텍스트필드 쓰면 텍스트랑 키보드나옴
+                              controller: _textEditingController,
+                              //텍스트필드에 들어가있는 텍스트를 플랫버튼을 누르면 print하게
+                              onSubmitted: _handleSubmitted,
+                              //Keyboard 판에서 ok를 눌렀을때도 데이터를 보낸다
+                              placeholder: '메세지를 입력해주세요',
+                              textAlign: TextAlign.center,
+                            )
+                          : TextField(
+                              //텍스트필드 쓰면 텍스트랑 키보드나옴
+                              controller: _textEditingController,
+                              //텍스트필드에 들어가있는 텍스트를 플랫버튼을 누르면 print하게
+                              onSubmitted: _handleSubmitted,
+                              //Keyboard 판에서 ok를 눌렀을때도 데이터를 보낸다
+                              decoration: InputDecoration(
+                                hintText: '메세지를 입력해주세요',
+                              ),
+                              textAlign: TextAlign.center,
+                            ),
                     ),
                     SizedBox(
                       width: 8.0,
                     ),
-                    FlatButton(
-                      onPressed: () {
-                        // print(_textEditingController.text);
-                        print(_chats);
-                        _handleSubmitted(_textEditingController.text);
-                      },
-                      child: Text('Send'),
-                      color: Colors.amberAccent,
-                    )
+                    Platform.isIOS//현재 플랫폼이 IOS이면 쿠퍼티노
+                        ? CupertinoButton(
+                            child: Text("send"),
+                            onPressed: () {
+                              if(_textEditingController.text.isEmpty){
+                                return;
+                              }else{
+                              _handleSubmitted(_textEditingController.text);
+                              }
+                            },
+                          )
+                        : IconButton(
+                            onPressed: () {
+                              // print(_textEditingController.text);
+                              print(_chats);
+                              if(!_textEditingController.text.isEmpty){
+                              _handleSubmitted(_textEditingController.text);
+                              }
+                            },
+                            icon: Icon(Icons.send),
+                            color: Colors.amberAccent,
+                          )
                   ],
                 ),
               ),
@@ -81,18 +114,18 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
-  Widget _buildItem(context,index,animation){
-    return ChatMessage(_chats[index],animation);
+  Widget _buildItem(context, index, animation) {
+    return ChatMessage(_chats[index], animation);
   }
 
   void _handleSubmitted(String text) {
-    var logger =Logger();
-    _textEditingController.clear();               //텍스트에디터를 클리어해주어 없애준다.
-    _chats.insert(0,text);
-    _animListKey.currentState.insertItem(0); //animate 키의 . 현재상태를 . insertitem 에 0번째 인덱스에 넣어준다라고 알려줌
+    var logger = Logger();
+    _textEditingController.clear(); //텍스트에디터를 클리어해주어 없애준다.
+    _chats.insert(0, text);
+    _animListKey.currentState
+        .insertItem(0); //animate 키의 . 현재상태를 . insertitem 에 0번째 인덱스에 넣어준다라고 알려줌
     // setState(() {
     // _chats.insert(0,newChat);
     // });
-
   }
 }
